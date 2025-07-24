d69b1e55126ee954fc9a9c82a7fb0de70cbe0d56
paulusaditya <222410103052@mail.unej.ac.id>
2025-07-24 11:05:54 +0700

    Refactor code structure for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MenuPage from "./pages/MenuPage";
 import PretestPage from "./pages/PretestPage";
 import Materi from "./pages/Materi";
 import Suhu from "./pages/materi/suhu";
+import Kalor from "./pages/materi/kalor";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/pretest" element={<PretestPage />} />
         <Route path="/materi" element={<Materi />} />
         <Route path="/materi/suhu" element={<Suhu />} />
+        <Route path="/materi/kalor" element={<Kalor />} />
       </Routes>
     </BrowserRouter>
   );
